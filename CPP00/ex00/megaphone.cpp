9370e6eaef246bb80724ba3608f107fe4281eabb
Atoomsnor <verschoor.rowan@gmail.com>
2025-09-22 15:59:48 +0200

    Better way to cast to string

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: roversch <roversch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/16 16:47:17 by roversch          #+#    #+#             */
-/*   Updated: 2025/09/18 11:56:54 by roversch         ###   ########.fr       */
+/*   Created: 2025/09/22 15:43:30 by roversch          #+#    #+#             */
+/*   Updated: 2025/09/22 15:58:55 by roversch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 int	main(int argc, char **argv)
 {
+
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
@@ -22,7 +23,7 @@ int	main(int argc, char **argv)
 	}
 	for (int i = 1; i < argc; i++)
 	{
-		std::string str(argv[i]);
+		std::string str = argv[i];
 		for (size_t j = 0; j < str.size(); j++)
 			std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(str[j])));
 	}
