fb21a0b4861efab6441bd5eb8988fb778bdd2588
Atoomsnor <verschoor.rowan@gmail.com>
2025-09-22 17:00:16 +0200

    Start of ex01, classes sure are weird exdee

---
+++ CPP00/ex01/inc/contact.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roversch <roversch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/22 16:21:11 by roversch          #+#    #+#             */
+/*   Updated: 2025/09/22 16:52:47 by roversch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <string>
+
+class Contact{
+	private:
+		std::string	fst_name_;
+		std::string	lst_name_;
+		std::string	nick_name_;
+		std::string	phone_nbr_;
+		std::string	dark_scrt_;
+	public:
+		
+};
+
+#endif
---
+++ CPP00/ex01/inc/phonebook.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roversch <roversch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/22 16:02:46 by roversch          #+#    #+#             */
+/*   Updated: 2025/09/22 16:44:00 by roversch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include "contact.hpp"
+
+class PhoneBook{
+	private:
+		Contact	contacts_[8];
+		bool	full_;
+	public:
+		
+};
+
+void	ye();
+
+#endif
