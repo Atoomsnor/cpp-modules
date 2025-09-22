466d0af4a186196f356b68da5fa0dead26976803
Atoomsnor <verschoor.rowan@gmail.com>
2025-09-22 15:42:20 +0200

    Initial commits

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roversch <roversch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/16 16:47:17 by roversch          #+#    #+#             */
+/*   Updated: 2025/09/18 11:56:54 by roversch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		std::string str(argv[i]);
+		for (size_t j = 0; j < str.size(); j++)
+			std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(str[j])));
+	}
+	std::cout << std::endl;
+	return (0);
+}
