0584902c060c8bcd024ec08d6fd97059b3415777
Atoomsnor <verschoor.rowan@gmail.com>
2025-09-25 11:16:37 +0200

    Input reading and start of class functions

--- CPP00/ex01/inc/phonebook.hpp
+++ CPP00/ex01/inc/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: roversch <roversch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 16:02:46 by roversch          #+#    #+#             */
-/*   Updated: 2025/09/22 16:44:00 by roversch         ###   ########.fr       */
+/*   Updated: 2025/09/25 10:48:09 by roversch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,12 @@
 class PhoneBook{
 	private:
 		Contact	contacts_[8];
+		int		count_;
 		bool	full_;
 	public:
-		
+		PhoneBook();
+		void addcontact();
+		void searchcontact();
 };
-
-void	ye();
 
 #endif
--- CPP00/ex01/src/phonebook.cpp
+++ CPP00/ex01/src/phonebook.cpp
@@ -6,16 +6,48 @@
 /*   By: roversch <roversch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 16:02:13 by roversch          #+#    #+#             */
-/*   Updated: 2025/09/22 16:32:24 by roversch         ###   ########.fr       */
+/*   Updated: 2025/09/25 10:52:43 by roversch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 #include <iostream>
 
+PhoneBook::PhoneBook() : count_(0), full_(false) {}
+
+void	PhoneBook::addcontact()
+{
+	std::cout << "Pretending to add a contact" << std::endl;
+	if (full_ == false)
+		count_++;
+	if (count_ == 8)
+		full_ = true;
+	std::cout << "Nr. of contacts: " << count_ << " Full? " << full_ << std::endl;
+}
+
+void	PhoneBook::searchcontact()
+{
+	std::cout << "Pretending to search a conctact" << std::endl;
+}
+
 int	main()
 {
-	std::cout << "phonebook" << std::endl;
-	ye();
+	PhoneBook pb;
+	std::string command;
+	
+	std::cout << "Please enter ADD, SEARCH or EXIT" << std::endl;
+	while (true)
+	{
+		std::cout << "> ";
+		std::getline(std::cin, command);
+		if (command == "EXIT")
+			break ;
+		else if (command == "ADD")
+			pb.addcontact();
+		else if (command == "SEARCH")
+			pb.searchcontact();
+		else
+			std::cout << "Unkown command. Please enter ADD, SEARCH or EXIT" << std::endl;
+	}
 	return (0);
 }
